d6362f5c1a85df7ed4f1bd6fbb1728ca9121fb5e
ioooi <ioooi@Isfahan.local>
2024-07-07 15:50:45 +0900

    .

--- src/api-config.js
+++ src/api-config.js
@@ -2,8 +2,8 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if(hostname == "localhost"){
-    backendHost = "http://localhost:8080";    
+if(hostname == "backend"){
+    backendHost = "http://192.168.49.2:38080";    
 }
 
 export const API_BASE_URL = `${backendHost}`;
